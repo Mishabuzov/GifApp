9361ebf3953e374aa27875e8a5efaeb1da185ed1
Mishabuzov <mikhaijl7@gmail.com>
2020-09-10 17:00:44 +0300

    added delay constant

--- app/src/main/java/ru/home/gifapp/Config.kt
+++ app/src/main/java/ru/home/gifapp/Config.kt
@@ -4,4 +4,6 @@ object Config {
     const val GIF_SERVICE_ENDPOINT = "https://developerslife.ru"
     const val JSON_PARAM_NAME = "json"
     const val JSON_PARAM_VALUE = true
+
+    const val SMALL_DELAY_MS = 1500L
 }
